78595933e5ede3919799488e7dbe748bab2ed152
Moritz Göckel <moritz.goeckel@gmail.com>
2018-05-07 19:55:24 +0200

    Reworked it a lot

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 #include <memory>
 #include <SFML/Graphics.hpp>
 #include <string>
+#include <chrono>
 
 class FieldItem
 {
   public:
+    FieldItem();
     int state = 0;
     int nextState = 0;
     std::unique_ptr<sf::RectangleShape> shape;
@@ -25,6 +27,8 @@ class FieldItem
 };
 
 std::unique_ptr<sf::Font> FieldItem::font = nullptr;
+
+FieldItem::FieldItem(){ }
 
 void FieldItem::setComment(std::string comment){
   if(this->text == nullptr){
@@ -80,23 +84,25 @@ int main()
   FieldItem::font = std::make_unique<sf::Font>();
   FieldItem::font->loadFromFile("VeraMono.ttf");
 
-  const int blockSize = 30;
-  const int xSize = 30, ySize = 30;
-  std::array<std::array<std::unique_ptr<FieldItem>, xSize>, ySize> field;
+  const int borderSize = 40;
+  const int blockSize = 20;
+  const int xSize = 40 + borderSize, ySize = 30 + borderSize;
+  std::array<std::array<FieldItem, xSize>, ySize> field;
 
-  sf::RenderWindow window(sf::VideoMode(xSize * blockSize, ySize * blockSize),
-   "Game of Life");
+  sf::RenderWindow window(sf::VideoMode((xSize - borderSize) * blockSize, (ySize - borderSize) * blockSize), "Game of Life");
 
-   for (int x = 0; x < field.size(); x++)
+  int offset = borderSize / 2 * blockSize;
+  for (int x = 0; x < field.size(); x++){
     for (int y = 0; y < field[x].size(); y++){
       auto rectangle = std::make_unique<sf::RectangleShape>(sf::Vector2f(blockSize, blockSize));
-      rectangle->setPosition(blockSize * x, blockSize * y);
-      field[x][y] = std::make_unique<FieldItem>(0, std::move(rectangle));
+      rectangle->setPosition(blockSize * x - offset, blockSize * y - offset);
+      field[x][y] = FieldItem(0, std::move(rectangle));
     }
+  }
 
   //Some patterns
 
-  field[0][0]->setState(1);
+  /*field[0][0]->setState(1);
   field[0][1]->setState(1);
   field[1][0]->setState(1);
   field[1][1]->setState(1);
@@ -104,22 +110,27 @@ int main()
   field[3][5]->setState(1);
   field[3][6]->setState(1);
   field[4][5]->setState(1);
-  field[4][6]->setState(1);
+  field[4][6]->setState(1);*/
 
   /*field[3 + 2][5 + 2]->setState(1);
   field[3 + 2][6 + 2]->setState(1);
   field[4 + 2][5 + 2]->setState(1);
   field[4 + 2][6 + 2]->setState(1);*/
 
-  field[10][10]->setState(1);
-  field[10][9]->setState(1);
-  field[10][8]->setState(1);
-  field[11][8]->setState(1);
-  field[8][9]->setState(1);
+  int midX = field.size() / 2;
+  int midY = field[0].size() / 2;
 
-  field[20][20]->setState(1);
-  field[21][20]->setState(1);
-  field[19][20]->setState(1);
+  field[midX + 0][midY + 0].setState(1);
+  field[midX + 0][midY - 1].setState(1);
+  field[midX + 0][midY - 2].setState(1);
+  field[midX + 1][midY - 2].setState(1);
+  field[midX - 1][midY - 1].setState(1);
+
+  /*field[20][20].setState(1);
+  field[21][20].setState(1);
+  field[19][20].setState(1);*/
+
+  long lastUpdate = 0;
 
   //Game Loop
   while (window.isOpen())
@@ -131,41 +142,57 @@ int main()
         window.close();
     }
 
-    //Tick
-    //Analyse and set next states
-    for (int x = 0; x < field.size(); x++)
-     for (int y = 0; y < field[x].size(); y++){
-       int aliveNeighbors = 0;
-       for(int xOffset = -1; xOffset <= 1; xOffset++)
-        for(int yOffset = -1; yOffset <= 1; yOffset++)
-          if(xOffset != 0 || yOffset != 0)
-            aliveNeighbors += (field[(x + xOffset) % field.size()][(y + yOffset) % field[x].size()]->state == 1 ? 1 : 0);
-        field[x][y]->setComment(std::string(" ") + std::to_string(aliveNeighbors));
+    long now = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::steady_clock::now().time_since_epoch()).count();
 
-        if(aliveNeighbors < 2 || aliveNeighbors > 3)
-          field[x][y]->setNextState(0);
-        else if(aliveNeighbors == 3)
-          field[x][y]->setNextState(1);
+    //Do tick
+    if(now - lastUpdate > 100){
+      lastUpdate = now;
+
+      //Analyse and set next states
+      for (int x = 0; x < field.size(); x++){
+        for (int y = 0; y < field[x].size(); y++){
+          int aliveNeighbors = 0;
+          for(int xOffset = -1; xOffset <= 1; xOffset++){
+            for(int yOffset = -1; yOffset <= 1; yOffset++){
+              if(xOffset != 0 || yOffset != 0 && x + xOffset >= 0 && x + xOffset < field.size() && y + yOffset >= 0 && y + yOffset < field[x].size()){
+                aliveNeighbors += field[x + xOffset][y + yOffset].state == 1 ? 1 : 0;
+                //Neighbors outside the field are state = 0
+              }
+            }
+          }
+
+          field[x][y].setComment(std::string(" ") + std::to_string(aliveNeighbors));
+
+          if(aliveNeighbors < 2 || aliveNeighbors > 3)
+            field[x][y].setNextState(0);
+          else if(aliveNeighbors == 3)
+            field[x][y].setNextState(1);
+        }
       }
 
-    //Transit into next states
-    for (int x = 0; x < field.size(); x++)
-     for (int y = 0; y < field[x].size(); y++)
-      field[x][y]->transitionState();
-
-    //Draw
-    window.clear();
-    int drawCount = 0;
-
-    for (int x = 0; x < field.size(); x++)
-     for (int y = 0; y < field[x].size(); y++){
-       if(field[x][y]->needsDrawing()){
-         field[x][y]->draw(window);
-         drawCount++;
-       }
+      //Transit into next states
+      for (int x = 0; x < field.size(); x++){
+        for (int y = 0; y < field[x].size(); y++){
+          field[x][y].transitionState();
+        }
       }
 
-    //std::cout << drawCount << " Tiles drawn" << std::endl;
+      //Draw
+      window.clear();
+      int drawCount = 0;
+
+      for (int x = 0; x < field.size(); x++){
+        for (int y = 0; y < field[x].size(); y++){
+          if(field[x][y].needsDrawing()){
+            field[x][y].draw(window);
+            drawCount++;
+          }
+        }
+      }
+
+      //End tick
+    }
+
     window.display();
   }
 
